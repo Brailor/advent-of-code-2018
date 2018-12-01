50a26ad395615b0567911d9fa6467a26ac0e6b37
Viktor Ohad <ygatravaz@protonmail.com>
2018-12-02 00:37:12 +0100

    part-2

--- one/lib/one.ex
+++ one/lib/one.ex
@@ -1,22 +1,39 @@
 defmodule One do
-
-  def read_input path do
+  def read_input(path) do
     {status, content} = File.read(path)
 
     case status do
-      :ok -> format_input(content)
+      :ok -> second_occurance(String.split(content))
       :error -> IO.puts("Cant read file!")
-   end
+    end
   end
 
-  def format_input string do
-    values = string |> String.split
-
-    result = Enum.reduce(values, 0, fn curr, res ->
+  def total_count(string) do
+    result =
+      Enum.reduce(string, 0, fn curr, res ->
         res + String.to_integer(curr)
       end)
-    IO.inspect result
+
+    IO.inspect(result)
+  end
+
+  def second_occurance(string, freq \\ 0, freq_map \\ %{0 => 1}) do
+    result =
+      Enum.reduce_while(string, {freq, freq_map}, fn curr, {freq, freq_map} ->
+        freq = freq + String.to_integer(curr)
+
+        cond do
+          Map.has_key?(freq_map, freq) -> {:halt, {:succ, freq}}
+          true -> {:cont, {freq, Map.put(freq_map, freq, 1)}}
+        end
+      end)
+
+    case result do
+      {:succ, freq} -> IO.inspect("The first match is at frequency: #{freq}")
+      {freq, freq_map} -> second_occurance(string, freq, freq_map)
+      true -> IO.puts("No duplicate frequency found!")
+    end
   end
 end
 
-One.read_input "lib/input.txt"
+One.read_input("lib/input.txt")
